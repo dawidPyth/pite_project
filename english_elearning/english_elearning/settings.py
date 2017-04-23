a2f540d0488257c62bbd92c9d7951944f9ad9424
Damian Swist <damian.swist@gmail.com>
2017-04-24 00:13:31 +0200

    adding init version of modules for transpations_app

--- english_elearning/english_elearning/settings.py
+++ english_elearning/english_elearning/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'translations_module',
+    'translations_module',
 ]
 
 MIDDLEWARE = [
