b7d32d04e4c2e2bf1660de58c0a86713d92a582e
Dawid Wełna <jan125djw@gmail.com>
2017-05-06 02:09:27 +0200

    email form trial

--- english_elearning/translations_module/urls.py
+++ english_elearning/translations_module/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^search_form/$', views.search_form),
+    url(r'^searched/$', views.search),
+    url(r'^searched/$', views.send_email),
 ]
--- english_elearning/translations_module/views.py
+++ english_elearning/translations_module/views.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+
 
 
 def post_list(request):
@@ -15,4 +18,23 @@ def search(request):
         message = 'You searched for: %r' % request.GET['q']
     else:
         message = 'You submitted an empty form.'
-    return HttpResponse(message)
+    c = {'message': message}
+    return render(request, 'searched.html', c)
+    
+
+
+def send_email(request):
+    subject = 'translation suggestion from user'
+    message = request.POST.get('message', '')
+    from_email = request.POST.get('from_email', '')
+    if subject and message and from_email:
+        try:
+            send_mail(subject, message, from_email, ['jan125djw@gmail.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+	return render(request, 'search_form.thml')        
+	#return HttpResponseRedirect('/search_form/')
+    else:
+        return HttpResponse('Make sure all fields are entered and valid.')
+
+
