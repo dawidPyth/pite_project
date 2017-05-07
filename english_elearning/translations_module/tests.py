bf0ebb431095afa9f5eb74b3ad6a27e63b51c8dd
Dawid Wełna <jan125djw@gmail.com>
2017-05-08 00:00:19 +0200

    simple email test

--- english_elearning/translations_module/tests.py
+++ english_elearning/translations_module/tests.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
+from django.core import mail
+from django.test import TestCase
+
+
+class EmailTest(TestCase):
+    EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+    def test_send_email(self):
+        mail.send_mail('Subject here', 'Here is the message.',
+            'python.projekt@o2.pl', ['jan125djw@gmail.com'],
+            fail_silently=False)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Subject here')
