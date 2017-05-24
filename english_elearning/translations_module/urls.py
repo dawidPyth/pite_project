2912fe420705c8e37a03ddbd9bee0fe4aa21cc4c
Klaudia <klaudia.g.1993@gmail.com>
2017-05-24 21:53:36 +0200

    views refactor + profile functionalities

--- english_elearning/translations_module/urls.py
+++ english_elearning/translations_module/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from views import DictionaryView
+from views import DictionaryView, ProfileView, PasswordView
 
 urlpatterns = [
     url(r'^dictionary/?$', login_required(DictionaryView.as_view()), name='Translator'),
+    url(r'^change_email/?$', login_required(ProfileView.as_view()), name='Email_change'),
+    url(r'^change_password/?$', login_required(PasswordView.as_view()), name='Password_change'),
 ]
