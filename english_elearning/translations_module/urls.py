6bf029816f6fce3f96bc6bb288159ed1a68cba7b
Dawid Wełna <jan125djw@gmail.com>
2017-05-30 17:53:44 +0200

    firstquiz.0.1

--- english_elearning/translations_module/urls.py
+++ english_elearning/translations_module/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
 from views import DictionaryView
+from views import QuizView
 
 urlpatterns = [
     url(r'^dictionary/?$', login_required(DictionaryView.as_view()), name='Translator'),
+    url(r'^quiz/?$', login_required(QuizView.as_view()), name='Quiz'),
 ]
--- english_elearning/translations_module/views.py
+++ english_elearning/translations_module/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import DetailView
-
+from random import randint
 from models import Words, Translations
 
 
@@ -32,3 +32,47 @@ class DictionaryView(DetailView):
 
         context = {'translated_words': translated_words}
         return render(request, 'dictionary.html', context)
+
+class QuizView(DetailView):
+
+    def get(self, request):
+
+        x = randint(4568, 9134)
+        request.session['x'] = x 
+        word_to_translate = []
+	words_tuples = list(Words.objects.filter(id=x))
+	word_to_translate = [the_tuple.word for the_tuple in words_tuples]
+        
+        context = {'print_word' : word_to_translate}
+        return render(request, 'quiz.html', context)
+        #return render(request, 'quiz.html', context={'quiz': ""})
+
+
+    def post(self, request):
+        translated_word = request.POST.get('word', "").lower()
+
+        word = list(Words.objects.filter(word=translated_word))
+
+
+        if 'x' in request.session:
+            x = request.session['x']  
+        else:
+            x = 9001     
+
+        count = 0
+        if word:
+            query_set = list(Translations.objects.filter(id_polish=word[0].id))
+            ids = [int(word.id_eng) for word in query_set]
+            print ids[0]
+
+            if x == ids[0]:
+                message = 'Correct!'
+                count = count + 1
+            else:
+                message = 'Incorrect answer!'
+
+        else:
+            message = "Incorrect input"
+        context = {'message' : message}
+        return render(request, 'quiz.html', context)
+
